7f78191f87505882aae87ab5106e1bb1fa3633bb
Niabag <gabain@hotmail.fr>
2025-06-10 07:15:13 +0200

    dsf

--- Backend/models/user.js
+++ Backend/models/user.js
@@ -84,6 +84,5 @@ UserSchema.methods.comparePassword = async function(password) {
   return await bcrypt.compare(password, this.password);
 };
 
-// Avoid OverwriteModelError in development by reusing existing model if loaded
 module.exports = mongoose.models.User || mongoose.model('User', UserSchema);
 
